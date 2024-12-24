24a9f30038bfa0dd700e14dbeb53333c8f215274
Pedro Lucas <pedro.lucaswpl@gmail.com>
2024-12-24 00:20:59 -0300

    feat: transições nas páginas

--- lib/routes.dart
+++ lib/routes.dart
@@ -6,11 +6,21 @@ class Routes {
   static const itemDetail = '/item/detail';
 
   static List<GetPage> dependencies = [
-    GetPage(name: Routes.menu, page: () => const MenuPage()),
-    GetPage(name: Routes.cart, page: () => const CartPage()),
+    GetPage(
+      name: Routes.menu,
+      page: () => const MenuPage(),
+      transition: Transition.topLevel,
+    ),
+    GetPage(
+      name: Routes.cart,
+      page: () => const CartPage(),
+      transition: Transition.topLevel,
+    ),
     GetPage(
       name: Routes.itemDetail,
       page: () => ItemDetailPage(item: Get.arguments),
+      transition: Transition.zoom,
+      transitionDuration: const Duration(milliseconds: 200),
     ),
   ];
 }
